cfb5077eea9a2d8e8b4c2bb1c3003f0b19440116
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2024-08-25 14:33:56 +1000

    Add files via upload

---
+++ Crusher Castle/luna.lua
@@ -0,0 +1,26 @@
+local onlinePlay = require("scripts/onlinePlay")
+
+local crusherMoveTime = 1200
+local crusherMoveDistance = 416
+
+local crusherMoveTimer = onlinePlay.createVariable("crusherMoveTimer","uint16",true,0)
+local crusherLayer
+
+function onStart()
+    crusherLayer = Layer.get("Crusher")
+end
+
+function onTick()
+    if crusherLayer ~= nil then
+        crusherLayer.pauseDuringEffect = false
+
+        if not crusherLayer:isPaused() then
+            local time = crusherMoveTime/math.pi/2
+
+            crusherLayer.speedY = math.sin(crusherMoveTimer.value/time)*crusherMoveDistance/time*0.5
+            crusherLayer.pauseDuringEffect = false
+            
+            crusherMoveTimer.value = (crusherMoveTimer.value + 1) % crusherMoveTime
+        end
+    end
+end
