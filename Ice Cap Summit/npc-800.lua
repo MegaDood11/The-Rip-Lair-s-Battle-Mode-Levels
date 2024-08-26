eb156c85a6d7d85df553a4561edd4291dbac2a9a
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2024-08-26 16:19:51 +1000

    Add files via upload

---
+++ Ice Cap Summit/npc-800.lua
@@ -0,0 +1,103 @@
+--NPCManager is required for setting basic NPC properties
+local npcManager = require("npcManager")
+
+--Create the library table
+local sampleNPC = {}
+--NPC_ID is dynamic based on the name of the library file
+local npcID = NPC_ID
+
+--Defines NPC config for our NPC. You can remove superfluous definitions.
+local sampleNPCSettings = {
+	id = npcID,
+	--Sprite size
+	gfxwidth = 96,
+	gfxheight = 64,
+	--Hitbox size. Bottom-center-bound to sprite size.
+	width = 96,
+	height = 64,
+	--Sprite offset from hitbox for adjusting hitbox anchor on sprite.
+	gfxoffsetx = 0,
+	gfxoffsety = 0,
+	--Frameloop-related
+	frames = 1,
+	framestyle = 0,
+	framespeed = 8, --# frames between frame change
+	--Movement speed. Only affects speedX by default.
+	speed = 1,
+	--Collision-related
+	npcblock = true,
+	npcblocktop = true, --Misnomer, affects whether thrown NPCs bounce off the NPC.
+	playerblock = true,
+	playerblocktop = true, --Also handles other NPCs walking atop this NPC.
+
+	nohurt=true,
+	nogravity = true,
+	noblockcollision = true,
+	nofireball=true,
+	noiceball=true,
+	noyoshi= true,
+	foreground = true
+}
+
+--Applies NPC settings
+npcManager.setNpcSettings(sampleNPCSettings)
+
+--Register the vulnerable harm types for this NPC. The first table defines the harm types the NPC should be affected by, while the second maps an effect to each, if desired.
+npcManager.registerHarmTypes(npcID,
+	{
+	}, 
+	{
+		[HARM_TYPE_OFFSCREEN]=800,
+	}
+);
+
+--Custom local definitions below
+local icechunk_break = Misc.resolveFile("icechunk_break.wav")
+
+--Register events
+function sampleNPC.onInitAPI()
+	npcManager.registerEvent(npcID, sampleNPC, "onTickNPC")
+	--npcManager.registerEvent(npcID, sampleNPC, "onTickEndNPC")
+	--npcManager.registerEvent(npcID, sampleNPC, "onDrawNPC")
+	--registerEvent(sampleNPC, "onNPCKill")
+end
+
+function sampleNPC.onTickNPC(v)
+	--Don't act during time freeze
+	if Defines.levelFreeze then return end
+	
+	local data = v.data
+	
+	--If despawned
+	if v.despawnTimer <= 0 then
+		--Reset our properties, if necessary
+		data.initialized = false
+		return
+	end
+
+	--Initialize
+	if not data.initialized then
+		--Initialize necessary data.
+		data.initialized = true
+	end
+
+	--Depending on the NPC, these checks must be handled differently
+	if v:mem(0x12C, FIELD_WORD) > 0    --Grabbed
+	or v:mem(0x136, FIELD_BOOL)        --Thrown
+	or v:mem(0x138, FIELD_WORD) > 0    --Contained within
+	then
+		--Handling
+	end
+	
+	--Execute main AI. This template just jumps when it touches the ground.
+	for _,p in ipairs(Player.get()) do
+		if p.x + p.width > v.x and p.x < v.x + v.width - 2 and p.y > v.y - p.height - 12 and p.y + p.height / 2 < v.y + v.height and p.speedY > 0 then
+			p.speedY = -8
+			SFX.play(icechunk_break)
+			v:kill(HARM_TYPE_OFFSCREEN)
+		end
+	end
+end
+
+--Gotta return the library table!
+return sampleNPC
