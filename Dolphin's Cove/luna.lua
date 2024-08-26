7147d0a7e844774a63d60d0fd84ace6656642683
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2024-08-26 14:16:45 +1000

    Add files via upload

--- Dolphin's Cove/luna.lua
+++ Dolphin's Cove/luna.lua
@@ -1,1 +1,71 @@
-local onlinePlay = require("scripts/onlinePlay")
+ local onlinePlayNPC = require("scripts/onlinePlay_npc")
+
+onlinePlayNPC.onlineHandlingConfig[459] = {
+	getExtraData = function(v)
+		local data = v.data._basegame
+		if not data.initialized then
+			return nil
+		end
+
+		return {
+			down = data.down,
+			type = data.type,
+			resttime = data.resttime,
+			gravitymultiplier = data.gravitymultiplier,
+			jumpspeed = data.jumpspeed,
+			effect = data.effect,
+			sound = data.sound,
+			friendlyrest = data.friendlyrest,
+		}
+	end,
+	setExtraData = function(v, receivedData)
+		local data = v.data._basegame
+		if not data.initialized then
+			return nil
+		end
+
+		data.down = receivedData.down
+		data.type = receivedData.type
+		data.resttime = receivedData.resttime
+		data.gravitymultiplier = receivedData.gravitymultiplier
+		data.jumpspeed = receivedData.jumpspeed
+		data.effect = receivedData.effect
+		data.sound = receivedData.sound
+		data.friendlyrest = receivedData.friendlyrest
+	end,
+}
+
+onlinePlayNPC.onlineHandlingConfig[460] = {
+	getExtraData = function(v)
+		local data = v.data._basegame
+		if not data.initialized then
+			return nil
+		end
+
+		return {
+			down = data.down,
+			type = data.type,
+			resttime = data.resttime,
+			gravitymultiplier = data.gravitymultiplier,
+			jumpspeed = data.jumpspeed,
+			effect = data.effect,
+			sound = data.sound,
+			friendlyrest = data.friendlyrest,
+		}
+	end,
+	setExtraData = function(v, receivedData)
+		local data = v.data._basegame
+		if not data.initialized then
+			return nil
+		end
+
+		data.down = receivedData.down
+		data.type = receivedData.type
+		data.resttime = receivedData.resttime
+		data.gravitymultiplier = receivedData.gravitymultiplier
+		data.jumpspeed = receivedData.jumpspeed
+		data.effect = receivedData.effect
+		data.sound = receivedData.sound
+		data.friendlyrest = receivedData.friendlyrest
+	end,
+}
