a26f3ba34e3da3025b466be2c7c119bd011da113
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2024-09-04 21:34:47 +1000

    Add files via upload

--- Subcon Fight Club/npc-133.lua
+++ Subcon Fight Club/npc-133.lua
@@ -3,8 +3,23 @@ local npcManager = require("npcManager")
 
 local npcID = NPC_ID
 
+local onlinePlayNPC = require("scripts/onlinePlay_npc")
+
 function bullet.onTickNPC(v)
+	if v.despawnTimer <= 0 then
+		return
+	end
+	
 	v.friendly = false
+	
+	local data = v.data
+
+	data.lifetime = (data.lifetime or 256) - 1
+
+	if data.lifetime <= 0 then
+		Effect.spawn(10,v.x + v.width*0.5 - 16,v.y + v.height*0.5 - 16)
+		onlinePlayNPC.forceKillNPC(v,HARM_TYPE_VANISH)
+	end
 end
 
 function bullet.onInitAPI()
