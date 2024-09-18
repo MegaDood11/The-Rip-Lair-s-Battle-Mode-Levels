ee76d08470af19a5ed87f0de02776b6e90308649
Murphmario <84686740+Murphmario@users.noreply.github.com>
2024-09-18 06:26:47 +0000

    Add files via upload

---
+++ Sleepy Battle Zone/luna.lua
@@ -0,0 +1,8 @@
+local blockRespawning = require("scripts/blockRespawning")
+local battlePlayer = require("scripts/battlePlayer")
+
+blockRespawning.defaultRespawnTime = 36*45
+
+function onTick()
+	
+end
