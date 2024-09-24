914fd775d8a1a13ab0bc3bc429b2c5a11e570e7d
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2024-09-24 17:28:53 +1000

    Add files via upload

---
+++ Block Buster/block-840.lua
@@ -0,0 +1,19 @@
+local blockmanager = require("blockmanager")
+local cp = require("clearpipe_opaque")
+
+local blockID = BLOCK_ID
+
+local block = {}
+
+blockmanager.setBlockSettings({
+	id = blockID,
+	noshadows = true,
+	width = 32,
+	height = 32,
+	small = true,
+})
+
+-- Up, down, left, right
+cp.registerPipe(blockID, "GATE", "HORZ", {false, false, true,  false})
+
+return block
