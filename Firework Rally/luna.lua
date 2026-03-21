1cd6bbd83d669826454055cffe1c96fbf5686cdb
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2026-03-21 15:39:55 +1100

    Add files via upload

--- Firework Rally/luna.lua
+++ Firework Rally/luna.lua
@@ -1,1 +1,15 @@
+local battleGeneral = require("scripts/battleGeneral")
 local onlinePlay = require("scripts/onlinePlay")
+local battleTimer = require("scripts/battleTimer")
+
+function battleGeneral.musicShouldBeSpedUp()
+	if battleGeneral.musicShouldSpeedUpFuncs[battleGeneral.mode] ~= nil and battleGeneral.musicShouldSpeedUpFuncs[battleGeneral.mode]() then
+        Audio.MusicChange(0, "Firework Rally/Mushroom City Final Lap.ogg")
+    end
+
+    if battleTimer.isActive and battleTimer.secondsLeft <= battleTimer.hurryTime then
+        Audio.MusicChange(0, "Firework Rally/Mushroom City Final Lap.ogg")
+    end
+    
+    return false
+end
