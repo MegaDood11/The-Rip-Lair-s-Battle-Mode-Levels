7f8bf9efe4b1ac8a2a50dcde0977fdfc36a039a5
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2026-03-21 14:57:43 +1100

    Add files via upload

---
+++ Red Ball Brawl/unused/npc-902.lua
@@ -0,0 +1,152 @@
+--[[
+	This template can be used to make your own custom NPCs!
+	Copy it over into your level or episode folder and rename it to use an ID between 751 and 1000. For example: npc-751.lua
+	Please pay attention to the comments (lines with --) when making changes. They contain useful information!
+	Refer to the end of onTickNPC to see how to stop the NPC talking to you.
+]]
+
+
+--NPCManager is required for setting basic NPC properties
+local npcManager = require("npcManager")
+local npcutils = require("npcs/npcutils")
+
+--Create the library table
+local sampleNPC = {}
+--NPC_ID is dynamic based on the name of the library file
+local npcID = NPC_ID
+
+--Defines NPC config for our NPC. You can remove superfluous definitions.
+local sampleNPCSettings = {
+	id = npcID,
+
+	-- ANIMATION
+	--Sprite size
+	gfxwidth = 64,
+	gfxheight = 320,
+	--Hitbox size. Bottom-center-bound to sprite size.
+	width = 64,
+	height = 320,
+	--Sprite offset from hitbox for adjusting hitbox anchor on sprite.
+	gfxoffsetx = 0,
+	gfxoffsety = 0,
+	--Frameloop-related
+	frames = 1,
+	framestyle = 0,
+	framespeed = 8, -- number of ticks (in-game frames) between animation frame changes
+
+	foreground = false, -- Set to true to cause built-in rendering to render the NPC to Priority -15 rather than -45
+
+	nohurt=true, -- Disables the NPC dealing contact damage to the player
+	nogravity = true,
+	noblockcollision = true,
+	notcointransformable = true, -- Prevents turning into coins when beating the level
+	nofireball = true,
+	noiceball = true,
+	noyoshi= true, -- If true, Yoshi, Baby Yoshi and Chain Chomp can eat this NPC
+	ignorethrownnpcs = true,
+	
+	--Various interactions
+	jumphurt = true, --If true, spiny-like (prevents regular jump bounces)
+}
+
+--Applies NPC settings
+npcManager.setNpcSettings(sampleNPCSettings)
+
+--Register events
+function sampleNPC.onInitAPI()
+	npcManager.registerEvent(npcID, sampleNPC, "onTickNPC")
+	npcManager.registerEvent(npcID, sampleNPC, "onDrawNPC")
+end
+
+function sampleNPC.onTickNPC(v)
+	--Don't act during time freeze
+	if Defines.levelFreeze then return end
+	
+	local data = v.data
+	
+	--If despawned
+	if v.despawnTimer <= 0 then
+		--Reset our properties, if necessary
+		data.initialized = false
+		return
+	end
+
+	--Initialize
+	if not data.initialized then
+		--Initialize necessary data.
+		data.initialized = true
+		data.rotation = 0
+		data.tilt = 0
+		if v.dontMove then v.direction = RNG.irandomEntry{-1,1} end
+	end
+	
+	for _,p in ipairs(Player.get()) do
+		if math.abs(p.x - 16 - (v.x - data.tilt)) <= 48 and math.abs(p.y - (v.y + -math.abs(data.tilt / 2) + v.height * 0.8)) <= 48 then
+			p:harm()
+		end
+	end
+	
+	npcutils.applyLayerMovement(v)
+	
+	data.tilt = data.tilt + (math.sin((lunatime.tick() + 69) / 48)) * -v.direction
+	
+	data.rotation = data.tilt
+end
+
+local function drawSprite(args) -- handy function to draw sprites
+	args = args or {}
+
+	args.sourceWidth  = args.sourceWidth  or args.width
+	args.sourceHeight = args.sourceHeight or args.height
+
+	if sprite == nil then
+		sprite = Sprite.box{texture = args.texture}
+	else
+		sprite.texture = args.texture
+	end
+
+	sprite.x,sprite.y = args.x,args.y
+	sprite.width,sprite.height = args.width,args.height
+
+	sprite.pivot = args.pivot or Sprite.align.TOPLEFT
+	sprite.rotation = args.rotation or 0
+
+	if args.texture ~= nil then
+		sprite.texpivot = args.texpivot or sprite.pivot or Sprite.align.TOPLEFT
+		sprite.texscale = args.texscale or vector(args.texture.width*(args.width/args.sourceWidth),args.texture.height*(args.height/args.sourceHeight))
+		sprite.texposition = args.texposition or vector(-args.sourceX*(args.width/args.sourceWidth)+((sprite.texpivot[1]*sprite.width)*((sprite.texture.width/args.sourceWidth)-1)),-args.sourceY*(args.height/args.sourceHeight)+((sprite.texpivot[2]*sprite.height)*((sprite.texture.height/args.sourceHeight)-1)))
+	end
+
+	sprite:draw{priority = args.priority,color = args.color,sceneCoords = args.sceneCoords or args.scene}
+end
+
+function sampleNPC.onDrawNPC(v)
+	local config = NPC.config[v.id]
+	local data = v.data
+
+	if v:mem(0x12A,FIELD_WORD) <= 0 or not data.rotation or data.rotation == 0 then return end
+
+	local priority = -45
+	if config.priority then
+		priority = -15
+	end
+
+	drawSprite{
+		texture = Graphics.sprites.npc[v.id].img,
+
+		x = v.x+(v.width/2)+config.gfxoffsetx,y = v.y+v.height-(config.gfxheight/2)+config.gfxoffsety,
+		width = config.gfxwidth,height = config.gfxheight,
+
+		sourceX = 0,sourceY = v.animationFrame*config.gfxheight,
+		sourceWidth = config.gfxwidth,sourceHeight = config.gfxheight,
+
+		priority = priority,rotation = data.rotation,
+		pivot = Sprite.align.CENTRE,sceneCoords = true,
+	}
+
+	npcutils.hideNPC(v)
+end
+
+
+--Gotta return the library table!
+return sampleNPC
