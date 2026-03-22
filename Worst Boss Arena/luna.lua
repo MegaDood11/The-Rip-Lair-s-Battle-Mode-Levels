728c7a5e553bb5704cbf75054af6f88e55698829
MegaDood11 <73924042+MegaDood11@users.noreply.github.com>
2026-03-22 14:09:44 +1100

    Add files via upload

--- Worst Boss Arena/luna.lua
+++ Worst Boss Arena/luna.lua
@@ -1,3 +1,12 @@
 local blockRespawning = require("scripts/blockRespawning")
+local battleGeneral = require("scripts/battleGeneral")
 
 blockRespawning.defaultRespawnTime = 24*30
+
+function onTick()
+	if battleGeneral.mode == battleGeneral.gameMode.PHANTO then
+		for _,v in ipairs(NPC.get(86)) do
+			v:kill(9)
+		end
+	end
+end
